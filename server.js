0210e70826c928c474edaa0a909fc7e167c1e6df
ijushten <jushten2@gmail.com>
2026-06-23 15:32:57 -0400

    move hompage to views folder

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "LessonAI" });
+});
 
 app.listen(PORT, () => {
   console.log(`LessonAI is running on port ${PORT}`);
